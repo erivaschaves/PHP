29a63a70918d46a20fc6865e6bc1d1b8ba0ece6e
ErivaldoChaves <erivas_12@hotmail.com>
2024-07-31 10:18:05 -0300

    exemplo PHP

---
+++ ex002/index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP</title>
+</head>
+<body>
+    <h1>Exemplo de PHP</h1>
+<!-- Mostrando dia e a hora-->
+    <?php
+        //Definindo data e hora para o Brasil
+        date_default_timezone_set("America/Sao_Paulo"); //GMT-03
+        echo "Hojé é dia " . date("d/M/Y");
+        echo " e a hora é " . date("G:i:s T");
+    ?>
+</body>
+</html>
