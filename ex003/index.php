8c0b7fffe94f33c630af37a088d080017530f3f9
ErivaldoChaves <erivas_12@hotmail.com>
2024-08-05 09:38:27 -0300

    Tipos primitivos

---
+++ ex003/index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Tipo Primitivos em PHP</title>
+</head>
+<body>
+    <h1>Teste de tipos primitivos</h1>
+    <?php
+        // // 0x = hexadecimal 0b = binário 0 = Octal
+        // $num = 0b1011;
+        // echo "O valor da variável é $num"
+        
+        // $v= 45;
+        // var_dump($v);
+
+        $num = (integer) 3e2; // 3 x 10(2) coerção
+        //echo "O valor é $num";
+        var_dump($num);
+
+
+
+    ?>
+    
+</body>
+</html>
