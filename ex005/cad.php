5bb37adda7c627376e473268f4da255bff59dbba
ErivaldoChaves <erivas_12@hotmail.com>
2024-08-13 15:48:32 -0300

    implementando o resultado

--- ex005/cad.php
+++ ex005/cad.php
@@ -7,23 +7,24 @@
     <link rel="stylesheet" href="style.css">
 </head>
 <body>
-    <header>
-        
-    </header>
     <main>
     <h1>Resultado Final</h1>
-        <?php
-          // Pegando numero através do "name" do input.
-          $numero = $_GET["numero"] ?? "sem numero"; // caso não haja numero.
-          $sucessor = $numero++;
-          $antecessor = $numero--;
-          echo "<p>O número escolhido foi <strong>$numero</strong>";
-          echo "<p> O seu antecessor é $antecessor";
-          echo "<p> O seu antecessor é $sucessor";       
-        ?>
+        <p>
+            <?php
+              // Pegando numero através do "name" do input.
+              //$num = 19;
+              //$a = $num - 1;
+              //$s = $num + 1;
+              $numero = $_GET["numero"] ?? "sem numero"; // caso não haja numero.
+              $sucessor = ($numero + 1);
+              $antecessor = ($numero - 1);
+              echo "<p>O número escolhido foi <strong>$numero</strong>";
+              echo "<br>O seu antecessor é $antecessor";
+              echo "<br>O seu sucessor é $sucessor";
+            ?>
+        </p>
         <!--Link para retornar a página anterior-->
-        <p><a href="javascript:history.go(-1)">Voltar</a></p>
-    </main>
+        <button onclick="javascript:window.location.href='index.html'">&#x2B05; Voltar</button>
     
 </body>
 </html>
