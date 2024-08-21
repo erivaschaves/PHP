c930893300cfcfa3acb997a9400fd2f0f6d4d42d
ErivaldoChaves <erivas_12@hotmail.com>
2024-08-21 10:36:11 -0300

    Salario minimo

---
+++ Desafios/D007/index.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Informe seu Salário</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <?php
+    $salario = 1380.00;
+    $valor = $_GET['val'] ?? 0;
+    $qtd = intdiv($valor, $salario);
+    $dif = $valor - ($qtd * $salario); 
+    ?>
+    <main>
+        <h1>Informe seu Salário</h1>
+       <form action="<?=$_SERVER['PHP_SELF']?>" method="get">
+        <label for="val">Salário(R$)</label>
+        <input type="number" name="val" id="val" value="<?=$valor?>">
+        <?php
+            echo "Considerando o salário mínimo de <strong>R$ $salario</strong>" 
+        ?>
+        <input type="submit" value="Calcular">
+       </form>
+
+    </main>
+    <section>
+        <h2>Resultado Final</h2>
+        <?php
+            print "<p>Quem recebe uma salário de $valor ganha <strong>$qtd salários mínimos</strong> + $dif</p>"; 
+        ?>        
+
+    </section>
+    
+</body>
+</html>
