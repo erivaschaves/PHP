e2fd4c86712c6696a05ffbd50652fdfa482b415a
ErivaldoChaves <erivas_12@hotmail.com>
2024-08-21 10:51:22 -0300

    salario minimo

--- Desafios/D007/index.php
+++ Desafios/D007/index.php
@@ -8,29 +8,28 @@
 </head>
 <body>
     <?php
-    $salario = 1380.00;
+    $salario = 1380;
     $valor = $_GET['val'] ?? 0;
     $qtd = intdiv($valor, $salario);
     $dif = $valor - ($qtd * $salario); 
     ?>
     <main>
-        <h1>Informe seu Salário</h1>
+    <h1>Informe seu Salário</h1>
        <form action="<?=$_SERVER['PHP_SELF']?>" method="get">
-        <label for="val">Salário(R$)</label>
-        <input type="number" name="val" id="val" value="<?=$valor?>">
-        <?php
-            echo "Considerando o salário mínimo de <strong>R$ $salario</strong>" 
-        ?>
-        <input type="submit" value="Calcular">
+            <label for="val">Salário(R$)</label>
+            <input type="number" name="val" id="val" value="<?=$valor?>">
+            <?php
+                echo "Considerando o salário mínimo de <strong>R\$".number_format($salario, 2, "," , ".")."</strong>" 
+            ?>
+            <input type="submit" value="Calcular">
        </form>
 
     </main>
     <section>
         <h2>Resultado Final</h2>
         <?php
-            print "<p>Quem recebe uma salário de $valor ganha <strong>$qtd salários mínimos</strong> + $dif</p>"; 
+            print "<p>Quem recebe uma salário de R\$".number_format($valor, 2, "," , ".")." ganha <strong>$qtd salários mínimos</strong> + R\$".number_format($dif, 2, "," , ".")."</p>"; 
         ?>        
-
     </section>
     
 </body>
