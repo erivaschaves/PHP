0e450c1ba6be122f76a1d2717c373d540ed5ad86
ErivaldoChaves <erivas_12@hotmail.com>
2024-08-22 09:32:31 -0300

    Cálculo de médias

---
+++ Desafios/D009/index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Médias Aritméticas</title>
+    <!--Carregando arquivo de estilo css-->
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <main>
+        <h1>Média Aritméticas</h1>
+    </main>
+
+    <section>
+        <h2></h2>
+    </section>
+    
+</body>
+</html>
