45249c843fc57e8829654e18710dc8b008a34f92
ErivaldoChaves <erivas_12@hotmail.com>
2024-08-26 09:57:43 -0300

    Calculadora de tempo

--- Desafios/D012/index.php
+++ Desafios/D012/index.php
@@ -4,29 +4,40 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Calculadora de Tempo</title>
+    <link rel="stylesheet" href="style.css">
 </head>
 <body>
+    <?php
+        $segundos = $_GET['num'] ?? 0;
+        $minutos =(integer)($segundos / 60);
+        $horas = (integer)($minutos / 60);
+        $dias = (integer)($horas/24);
+        $semanas = (integer)($dias/7);
+        $totDias = (($semanas * 7) - $dias) * -1;
+        $totHoras = (($dias * 24) - $horas) * -1;
+        $totMinutos = (($horas * 60) - $minutos) * -1;
+        $totSegundos = (($minutos * 60) - $segundos) * -1;
+    ?>
     <!--Criando formulário SERVER PHP_SELF e inputs-->
     <main>
         <h1>Calculadora de Tempo</h1>
-        <form action="<?=$_SERVER['PHP_SELF']?>">
+        <form action="<?=$_SERVER['PHP_SELF']?>"method = "get">
             <label for="num">Qual é o total de segundos?</label>
             <input type="number" name="num" id="num">
             <input type="submit" value="Calcular">
-
         </form>
     </main>
 
     <section>
         <h2>Totalizando Tudo</h2>
         <?php
-            echo "analisando o valor que você digitou, <strong>450 segundos</strong> equivalem a um total de:
+            echo "Analisando o valor que você digitou, <strong>".number_format($segundos, 0, "." , "." )." segundos</strong> equivalem a um total de:
             <ul>
-                <li>0 semanas</li>
-                <li>0 dias</li>
-                <li>0 horas</li>
-                <li>0 minutos</li>
-                <li>0 segundos</li>
+                <li>$semanas semanas</li>
+                <li>$totDias dias</li>
+                <li>$totHoras horas</li>
+                <li>$totMinutos minutos</li>
+                <li>$totSegundos segundos</li>
             </ul>" 
         ?>
     </section>
