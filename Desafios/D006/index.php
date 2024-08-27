a999f8f03fe0a711babeb7d2d836db43b89b55cc
ErivaldoChaves <erivas_12@hotmail.com>
2024-08-27 09:21:08 -0300

    Anatomia de uma divisão

--- Desafios/D006/index.php
+++ Desafios/D006/index.php
@@ -4,22 +4,23 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Anatomia de uma Divisão</title>
+    <!--Carregando arquivo css -->
     <link rel="stylesheet" href="style.css">
 </head>
 <body>
     <?php
         // Capturando os dados do Formulário Retroalimentado
         $valor1 = $_GET['v1']?? 0;
-        $valor2 = $_GET['v2']?? 0;
+        $valor2 = $_GET['v2']?? 1;
     ?>
 <main>
     <h1>Anatomia de uma Divisão</h1>
     <!--Criando formulário PHP_SELF-->
     <form action="<?=$_SERVER['PHP_SELF']?>" method="get">
         <label for="v1">Dividendo</label>
-        <input type="number" name="v1" id="v1" value="<?=$valor1?>">
+        <input type="number" name="v1" id="v1" min="0" value="<?=$valor1?>">
         <label for="v2">Divisor</label>
-        <input type="number" name="v2" id="v2" value="<?=$valor2?>">
+        <input type="number" name="v2" id="v2" min = 1 value="<?=$valor2?>">
         <input type="submit" value="Analisar">
         
     </form>
@@ -36,11 +37,11 @@
     <table class="divisao">
             <tr>
                 <td><?=$valor1?></td>
-                <td><?=$divisao?></td>
+                <td><?=$valor2?></td>
             </tr>
             <tr>
                 <td><?=$resto?></td>
-                <td><?=$valor2?></td>
+                <td><?=$divisao?></td>
             </tr>    
     </table>
 </section> 
